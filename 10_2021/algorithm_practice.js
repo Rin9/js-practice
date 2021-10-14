99af68afa29d8edf6535f4eba185d6e93225d5ed
Rin9 <formygame38@gmail.com>
2021-10-13 22:19:27 -0400

    more practice

--- 10_2021/algorithm_practice.js
+++ 10_2021/algorithm_practice.js
@@ -836,3 +836,85 @@ function cakes(recipeObj, ingredientsObj){
   return minCount;
 
 }
+
+
+/**
+==========================
+
+21. Human Readable Time
+
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+
+==========================
+**/
+
+
+function humanReadable(seconds) {
+  // TODO
+  let result = "";
+  let hourNum, minuteNum, secondNum;
+  hourNum = Math.floor(seconds / 3600);
+  seconds -= hourNum * 3600;
+  if(hourNum.toString().length < 2){
+    result += ("0" + hourNum + ":");
+  }else{
+    result += (hourNum + ":");
+  }
+
+  minuteNum = Math.floor(seconds / 60);
+  seconds -= minuteNum * 60;
+
+  if(minuteNum.toString().length < 2){
+    result += ("0" + minuteNum + ":");
+  }else{
+    result += (minuteNum + ":");
+  }
+
+  secondNum = Math.floor(seconds / 1);
+  if(secondNum.toString().length < 2){
+    result += ("0" + secondNum);
+  }else{
+    result += (secondNum + "");
+  }
+
+  return result;
+}
+
+
+/* 
+
+best solution
+
+function humanReadable(seconds) {
+var hours = parseInt( seconds / 3600 ) ;
+var minutes = parseInt( seconds / 60 ) % 60;
+var seconds = seconds % 60;
+
+var pad = function(val){
+  return val < 10 ?"0"+val:val;
+}
+
+return pad(hours) + ":" +pad(minutes) + ":" + pad(seconds);
+}
+
+*/
+
+
+/**
+==========================
+
+22. Rot13
+
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+==========================
+**/
