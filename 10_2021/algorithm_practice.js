bc88a842e2cd396c00de3eed4d82ab13c6fab646
Rin9 <formygame38@gmail.com>
2021-10-14 16:06:11 -0400

    3d-form practice

--- 10_2021/algorithm_practice.js
+++ 10_2021/algorithm_practice.js
@@ -910,11 +910,74 @@ return pad(hours) + ":" +pad(minutes) + ":" + pad(seconds);
 /**
 ==========================
 
-22. Rot13
+22. A Chain adding function
 
-ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+We want to create a function that will add numbers together when called in succession.
 
-Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+add(1)(2);
+// returns 3
+We also want to be able to continue to add numbers to our chain.
+
+add(1)(2)(3); // 6
+add(1)(2)(3)(4); // 10
+add(1)(2)(3)(4)(5); // 15
+and so on.
+
+A single call should return the number passed in.
+
+add(1); // 1
+We should be able to store the returned values and reuse them.
+
+var addTwo = add(2);
+addTwo; // 2
+addTwo + 5; // 7
+addTwo(3); // 5
+addTwo(3)(5); // 10
+We can assume any number being passed in will be valid whole number.
+==========================
+**/
+
+function add(n){
+  var fn = function(x) {
+    return add(n + x);
+  };
+  
+  fn.valueOf = function() {
+    return n;
+  };
+  
+  return fn;
+}
+
+/**
+==========================
+
+23. My smallest code interpreter (aka Brainf**k)
+
+Inspired from real-world Brainf**k, we want to create an interpreter of that language which will support the following instructions:
+
+> increment the data pointer (to point to the next cell to the right).
+< decrement the data pointer (to point to the next cell to the left).
++ increment (increase by one, truncate overflow: 255 + 1 = 0) the byte at the data pointer.
+- decrement (decrease by one, treat as unsigned byte: 0 - 1 = 255 ) the byte at the data pointer.
+. output the byte at the data pointer.
+, accept one byte of input, storing its value in the byte at the data pointer.
+[ if the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+] if the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
+The function will take in input...
+
+the program code, a string with the sequence of machine instructions,
+the program input, a string, eventually empty, that will be interpreted as an array of bytes using each character's ASCII code and will be consumed by the , instruction
+... and will return ...
+
+the output of the interpreted code (always as a string), produced by the . instruction.
+Implementation-specific details for this Kata:
+
+Your memory tape should be large enough - the original implementation had 30,000 cells but a few thousand should suffice for this Kata
+Each cell should hold an unsigned byte with wrapping behavior (i.e. 255 + 1 = 0, 0 - 1 = 255), initialized to 0
+The memory pointer should initially point to a cell in the tape with a sufficient number (e.g. a few thousand or more) of cells to its right. For convenience, you may want to have it point to the leftmost cell initially
+You may assume that the , command will never be invoked when the input stream is exhausted
+Error-handling, e.g. unmatched square brackets and/or memory pointer going past the leftmost cell is not required in this Kata. If you see test cases that require you to perform error-handling then please open an Issue in the Discourse for this Kata (don't forget to state which programming language you are attempting this Kata in).
 
 ==========================
 **/
